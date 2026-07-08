f7423ed6ec2a7abba0f8469878065185a763a827
Dmitry Shirokov <Aka-mant@mail.ru>
2026-07-08 20:51:51 +0300

    Правка модели для Profile

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -157,10 +158,6 @@ class User(AbstractUser):
             UserRole.USER,
             UserRole.PREMIUM,
         )
-
-from django.conf import settings
-from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 class Profile(models.Model):
