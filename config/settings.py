ea396f8d3327b19c06765d390b00de8cff960e7b
Dmitry Shirokov <Aka-mant@mail.ru>
2026-07-17 21:29:32 +0300

    Добавил fixtures, и base.html V0.002

--- config/settings.py
+++ config/settings.py
@@ -160,10 +160,13 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-STATIC_URL = 'static/'
-STATIC_DIRS = [
-    BASE_DIR / 'static',
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
